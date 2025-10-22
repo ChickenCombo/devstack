9e42e071978f139d1badfbb8f1f2bf2a06a6f33a
Lester Cuadra <lester.cuadra24@gmail.com>
2025-10-22 12:08:10 +0800

    feat(server): structured logging

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,35 +5,51 @@ import { appRouter } from "@devstack/api/routers/index";
 import { auth } from "@devstack/auth";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { logger } from "hono/logger";
+import { pinoLogger } from "hono-pino";
+import pino from "pino";
+import pretty from "pino-pretty";
+import type { AppBindings } from "./types";
 
-const app = new Hono();
+const app = new Hono<AppBindings>();
 
-app.use(logger());
 app.use(
-	"/*",
-	cors({
-		origin: process.env.CORS_ORIGIN as string,
-		allowMethods: ["GET", "POST", "OPTIONS"],
-		allowHeaders: ["Content-Type", "Authorization"],
-		credentials: true,
-	}),
+  pinoLogger({
+    pino: pino(
+      { level: process.env.LOG_LEVEL as string },
+      (process.env.NODE_ENV as string) === "production" ? undefined : pretty()
+    ),
+    http: {
+      reqId: () => crypto.randomUUID(),
+    },
+  })
+);
+
+app.use(
+  "/*",
+  cors({
+    origin: process.env.CORS_ORIGIN as string,
+    allowMethods: ["GET", "POST", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
 );
 
 app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
 
 app.use(
-	"/trpc/*",
-	trpcServer({
-		router: appRouter,
-		createContext: (_opts, context) => {
-			return createContext({ context });
-		},
-	}),
+  "/trpc/*",
+  trpcServer({
+    router: appRouter,
+    createContext: (_opts, context) => {
+      return createContext({ context });
+    },
+  })
 );
 
 app.get("/", (c) => {
-	return c.text("OK");
+  console.log(process.env.NODE_ENV);
+
+  return c.text("OK");
 });
 
 export default app;
